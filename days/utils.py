26965ca3f21e354c2528f25aab634106e5f1a421
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2022-12-04 00:11:48 -0500

    add colors

--- days/utils.py
+++ days/utils.py
@@ -1,17 +1,33 @@
-import pathlib
 import sys
 import time
+import traceback
+from pathlib import Path
 from typing import Any, Callable
 
 import requests
 
-with open(pathlib.Path(__file__).parent.parent / "token.txt", "r") as fp:
-    SESSION_TOKEN = fp.read()
+RESET = "\u001b[0m"
+BLUE = "\u001b[34m"
+RED = "\u001b[31m"
+GREEN = "\u001b[32m"
+CYAN = "\u001b[36m"
+
+
+PATH = Path(__file__).parent.parent / "token.txt"
+if PATH.exists():
+    with open(PATH, "r") as fp:
+        SESSION_TOKEN = fp.read()
+else:
+    print(
+        "warning: no `token.txt` file detected in the root directory, you will be unable to get data from aoc's website."
+    )
+
 
 def get_day_input_from_aoc(day: int, name: str) -> None:
-    file = pathlib.Path(__file__).parent.parent / "data" / f"day_{name}.txt"
+    file = Path(__file__).parent.parent / "data" / f"day_{name}.txt"
     with requests.get(
-        f"https://adventofcode.com/2022/day/{day}/input", cookies={"session": SESSION_TOKEN}
+        f"https://adventofcode.com/2022/day/{day}/input",
+        cookies={"session": SESSION_TOKEN},
     ) as res:
         with open(file, "w") as fp:
             fp.write(res.text)
@@ -24,14 +40,26 @@ def get_day_input(day: str) -> str:
 
 def time_and_print_result(part: str, callable: Callable[..., Any]):
     start = time.perf_counter()
-    result = callable()
+
+    try:
+        result = callable()
+    except Exception as exc:
+        traceback_text = "".join(
+            traceback.format_exception(type(exc), exc, exc.__traceback__, 4)
+        )
+        print(f"{RED}ERROR {BLUE}Part {part}: {RESET}\n{traceback_text}")
+        return
+
     end = time.perf_counter()
     took = (end - start) * 1000
-    print(f"Part {part}: {result} [took {took:.2f} ms]")
+
+    print(f"{BLUE}Part {part}: {GREEN}{result}{RESET} [took {CYAN}{took:.2f}ms{RESET}]")
+
 
 # Call this script with (int)day (string)output
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) != 2:
         raise Exception("args need to be in the form of (day), (name).")
+
     get_day_input_from_aoc(args[0], args[1])
